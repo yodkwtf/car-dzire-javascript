b857bfa4638ac8e4f96396b0f06e62d42271658d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-11-02 21:28:51 +0530

    completed top bar animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const topBar = document.getElementById('top-bar');
+
+// Handle top bar on scroll
+const handleTopBar = () => {
+  const isTop = window.scrollY === 0;
+  topBar.classList.toggle('visible-top-bar', isTop);
+  topBar.classList.toggle('hidden-top-bar', !isTop);
+};
+
+// # Event Listeners
+window.addEventListener('scroll', () => requestAnimationFrame(handleTopBar)); // save resources and improve performance
