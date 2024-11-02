74a70d870c85cd4606593d4faa07cd2d744c0544
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-11-02 21:53:13 +0530

    implemented interior and exterior color change

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+export const EXTERIOR_IMAGES = {
+  // TODO: Change images and the car model
+  'Stealth Grey': './images/model-y-stealth-grey.jpg',
+  'Pearl White': './images/model-y-pearl-white.jpg',
+  'Deep Blue': './images/model-y-deep-blue-metallic.jpg',
+  'Solid Black': './images/model-y-solid-black.jpg',
+  'Ultra Red': './images/model-y-ultra-red.jpg',
+  'Quick Silver': './images/model-y-quicksilver.jpg',
+};
+
+export const INTERIOR_IMAGES = {
+  // TODO: Change model
+  Dark: './images/model-y-interior-dark.jpg',
+  Light: './images/model-y-interior-light.jpg',
+};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+import { EXTERIOR_IMAGES, INTERIOR_IMAGES } from './constants.js';
+
 // # Selectors
 const topBar = document.getElementById('top-bar');
-const exteriorColorWrapper = document.getElementById('exterior-buttons');
-const interiorColorWrapper = document.getElementById('interior-buttons');
+const exteriorColorDiv = document.getElementById('exterior-buttons');
+const interiorColorDiv = document.getElementById('interior-buttons');
 const exteriorImage = document.getElementById('exterior-image');
 const interiorImage = document.getElementById('interior-image');
 
@@ -26,12 +28,25 @@ const handleColorButtonClick = (event) => {
   // If no button is clicked, exit the function
   if (!clickedBtn) return;
 
+  // Highlight the selected button
   const allButtons = event.currentTarget.querySelectorAll('button');
   allButtons.forEach((btn) => btn.classList.remove('btn-selected'));
   clickedBtn.classList.add('btn-selected');
+
+  // Change exterior image
+  if (event.currentTarget === exteriorColorDiv) {
+    const clickedColor = clickedBtn.querySelector('img').alt;
+    exteriorImage.src = EXTERIOR_IMAGES[clickedColor];
+  }
+
+  // Change interior image
+  if (event.currentTarget === interiorColorDiv) {
+    const clickedColor = clickedBtn.querySelector('img').alt;
+    interiorImage.src = INTERIOR_IMAGES[clickedColor];
+  }
 };
 
 // # Event Listeners
 window.addEventListener('scroll', () => requestAnimationFrame(handleTopBar)); // save resources and improve performance
-exteriorColorWrapper.addEventListener('click', handleColorButtonClick);
-interiorColorWrapper.addEventListener('click', handleColorButtonClick);
+exteriorColorDiv.addEventListener('click', handleColorButtonClick);
+interiorColorDiv.addEventListener('click', handleColorButtonClick);
